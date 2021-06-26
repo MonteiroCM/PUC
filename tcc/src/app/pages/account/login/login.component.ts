5ae43128c75c26f4e0264c6163a58be13620ab04
Monteiro <clayton.monteiro@gmail.com>
2021-06-26 19:48:51 -0300

    login

--- tcc/src/app/pages/account/login/login.component.ts
+++ tcc/src/app/pages/account/login/login.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AccountService } from '../shared/account.service';
+import { AccountService } from './../shared/account.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    login = {
-      email: '',
-      password: ''
-    }
+  login = {
+    email: '',
+    password: ''
+  };
+
   constructor(
     private accountService: AccountService,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   async onSubmit() {
     try {
-      const result = await this.accountService.login(this.login)
-      console.log(`Login efetuado: ${result}`)
+      const result = await this.accountService.login(this.login);
+      console.log(`Login efetuado: ${result}`);
 
-      this.router.navigate([''])
-    }catch (error) {
-      console.error(error)
+      this.router.navigate(['']);
+    } catch (error) {
+      console.error(error);
     }
   }
-
 }
